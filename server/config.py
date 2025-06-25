60d4fb0b4e41daea02734e1ee9d6dc59ad61baee
petermtgi <3sjk0krrr@gmail.com>
2025-06-25 15:37:41 +0300

    update

--- server/config.py
+++ server/config.py
@@ -11,9 +11,15 @@ migrate = Migrate()
 jwt = JWTManager()
 
 class Config:
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME", "two_late_show_api")
+
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret") 
--- server/controllers/__init__.py
+++ server/controllers/__init__.py
@@ -1,4 +1,4 @@
-from .auth_controller import bp as auth_controller
-from .guest_controller import bp as guest_controller
-from .episode_controller import bp as episode_controller
-from .appearance_controller import bp as appearance_controller
+from .auth_controller import auth_bp as auth_controller
+from .guest_controller import guest_bp as guest_controller
+from .episode_controller import episode_bp as episode_controller
+from .appearance_controller import appearance_bp as appearance_controller
