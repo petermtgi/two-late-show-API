36727b4aba44d5115bd77f1ddc263aeb6bf19239
petermtgi <3sjk0krrr@gmail.com>
2025-06-25 13:04:37 +0300

    updated

---
+++ server/models/guest.py
@@ -0,0 +1,7 @@
+from server.app import db
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
